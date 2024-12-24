6f247add15cc9207d868b6629edf8dd2ebf70de2
BrayanFJ <Brayan.fernandesjulio@gmail.com>
2024-12-23 21:50:31 -0300

    commit

---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScreenSound.Banco
+{
+    internal class ScreenSoundContext : DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = ScreenSound; Integrated Security = True; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        //public SqlConnection obterConexao()
+        //{
+        //    return new SqlConnection(connectionString);
+        //}
+
+      
+    }
+}
